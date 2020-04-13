ebc8722c838e2a0069efafd2c37d245bc082aefc
王水玲 <wang.shuiling@rongzer.com>
2020-04-13 15:19:13 +0800

    框架优化

--- src/components/layout/layout.vue
+++ src/components/layout/layout.vue
@@ -164,7 +164,7 @@ export default {
         align-items: center;
         .logo {
             padding: 0 30px;
-            background-color: var(--theme-color);
+            background-color: #2d8cf0;
             color: #fff;
             line-height: 46px;
             height: 100%;
