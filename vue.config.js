a1c8daf4407fa642496638fac2d884297415aaba
王水玲 <wang.shuiling@rongzer.com>
2020-04-14 17:17:27 +0800

    框架调整

--- vue.config.js
+++ vue.config.js
@@ -53,9 +53,9 @@ module.exports = {
         requireModuleExtension: true
     },
     chainWebpack: config => {
-        // config.optimization.splitChunks({
-        //     chunks: 'all'
-        // });
+        config.optimization.splitChunks({
+            chunks: 'all'
+        });
 
         if (isProduction) {
             config.plugin('html').tap(args => {
@@ -111,15 +111,15 @@ module.exports = {
             );
             config.plugins = [...config.plugins, ...plugins];
 
-            // config.performance = {
-            //     hints: 'warning', // 枚举
-            //     maxAssetSize: 30000000, // 整数类型（以字节为单位）
-            //     maxEntrypointSize: 50000000, // 整数类型（以字节为单位）
-            //     assetFilter: function(assetFilename) {
-            //         // 提供资源文件名的断言函数
-            //         return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
-            //     }
-            // };
+            config.performance = {
+                hints: 'warning', // 枚举
+                maxAssetSize: 30000000, // 整数类型（以字节为单位）
+                maxEntrypointSize: 50000000, // 整数类型（以字节为单位）
+                assetFilter: function(assetFilename) {
+                    // 提供资源文件名的断言函数
+                    return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+                }
+            };
         }
     }
 };
